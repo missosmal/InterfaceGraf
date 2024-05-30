ee9383201dd37ad7960cba25cd5024331440720b
mynirka1234 <mahmutovamunira0@gmail.com>
2024-05-30 17:12:37 +0500

    Выполненная практическая работа на оценку удовлетворительно.Graf

--- app/src/main/java/com/yarkov/interface_graf/MainActivity.java
+++ app/src/main/java/com/yarkov/interface_graf/MainActivity.java
@@ -1,7 +1,5 @@
 package com.yarkov.interface_graf;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
@@ -10,7 +8,6 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.MultiAutoCompleteTextView;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -25,56 +22,72 @@ import java.util.Date;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public List<Note> listOfNotes = new ArrayList<>();
 
-    public void OpenNote(View view)
-    {
+    public void OpenNote(View view) {
         setContentView(R.layout.note);
     }
+    int index = -1;
 
-    public class notes{
-        public String name;
-        public String text;
-        public String date;
-    }
-    public List<notes> list_nites = new ArrayList<>();
-    public void AddNote(View view)
-    {
-        notes new_notes = new notes();
-        EditText e_name = findViewById(R.id.editTextTextPersonName);
-        new_notes.name = e_name.getText().toString();
-        MultiAutoCompleteTextView e_text = findViewById(R.id.multiAutoCompleteTextView);
-        new_notes.text = e_text.getText().toString();
+    public void AddNote(View view){
+        Note newNote = new Note();
+        EditText name = findViewById(R.id.editTextTextPersonName);
+        MultiAutoCompleteTextView text = findViewById(R.id.multiAutoCompleteTextView);
+        newNote.name = name.getText().toString();
+        newNote.text = text.getText().toString();
 
         Date dateNow = new Date();
         SimpleDateFormat formatForDateNow = new SimpleDateFormat("yyyy.MM.dd");
-        new_notes.date = formatForDateNow.format(dateNow);
-
-        list_nites.add(new_notes);
+        newNote.date = formatForDateNow.format(dateNow);
+        if(index == -1)
+            listOfNotes.add(newNote);
+        else{
+            listOfNotes.set(index, newNote);
+            index = -1;
+        }
         setContentView(R.layout.activity_main);
         onLoad();
     }
-    public void onLoad(){
-        LinearLayout parent = findViewById(R.id.parent);
 
-        System.out.println(list_nites.size());
-        for (int i = 0; i < list_nites.size(); i++) {
+    public void OpenNotes(View view)
+    {
+        int id = (int) view.getTag();
+        setContentView(R.layout.note);
+        index = id;
+        EditText name = findViewById(R.id.editTextTextPersonName);
+        name.setText(listOfNotes.get(id).name);
 
+        MultiAutoCompleteTextView text = findViewById(R.id.multiAutoCompleteTextView);
+        text.setText(listOfNotes.get(id).text);
+    }
+
+    public void onLoad()
+    {
+        LinearLayout parrent = findViewById(R.id.parrent);
+        parrent.removeAllViews();
+        System.out.print(listOfNotes.size());
+
+        for(int i = 0; i < listOfNotes.size(); i++) {
+
+            final int finalI = i;
             LinearLayout ll = new LinearLayout(this);
             ll.setOrientation(LinearLayout.HORIZONTAL);
-            RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
-                    RelativeLayout.LayoutParams.MATCH_PARENT,
-                    RelativeLayout.LayoutParams.MATCH_PARENT
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT
             );
+
             ll.setLayoutParams(params);
+            ll.setTag(i);
+            ll.setOnClickListener(this::OpenNotes);
 
             ImageView iv = new ImageView(this);
-            iv.setImageResource(R.drawable.icon);
+            iv.setImageResource(R.drawable.file_icon );
             LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(150, 150);
             iv.setLayoutParams(layoutParams);
             iv.setPadding(20, 20, 20, 20);
@@ -84,24 +97,44 @@ public class MainActivity extends AppCompatActivity {
             ll_ver.setLayoutParams(params);
 
             TextView tv_name = new TextView(this);
-            tv_name.setText(list_nites.get(i).name);
-            RelativeLayout.LayoutParams params_tv = new RelativeLayout.LayoutParams(
-            RelativeLayout.LayoutParams.MATCH_PARENT,
-            RelativeLayout.LayoutParams.WRAP_CONTENT
+            tv_name.setText(listOfNotes.get(i).name);
+            LinearLayout.LayoutParams params_tv = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT
             );
+
             tv_name.setLayoutParams(params_tv);
             tv_name.setTextColor(Color.BLACK);
             tv_name.setTextSize(18);
 
             TextView tv_data = new TextView(this);
-            tv_data.setText(list_nites.get(i).date);
+            tv_data.setText(listOfNotes.get(i).date);
             tv_data.setLayoutParams(params_tv);
             tv_data.setTextColor(Color.GRAY);
 
-            parent.addView(ll);
+            Button btn_delete = new Button(this);
+            btn_delete.setText("Delete");
+            LinearLayout.LayoutParams btn_params = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT
+            );
+            btn_delete.setLayoutParams(btn_params);
+            btn_delete.setOnClickListener(v -> {
+                listOfNotes.remove(listOfNotes.get(finalI));
+                onLoad();
+            });
+
+            parrent.addView(ll);
             ll.addView(iv);
             ll.addView(ll_ver);
             ll_ver.addView(tv_name);
             ll_ver.addView(tv_data);
+            ll_ver.addView(btn_delete);
+        }
+    }
+    public class Note {
+        public String name;
+        public String text;
+        public String date;
     }
 }
